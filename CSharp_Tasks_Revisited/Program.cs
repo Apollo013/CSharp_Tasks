c6cf0d907fe400ca1139a9b86a8c02ffe197f940
Paul <paul.j.millar.1971@gmail.com>
2016-10-01 14:14:14 +0100

    Add Race Condition Senario To Revisited Project

---
+++ CSharp_Tasks_Revisited/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Tasks_Revisited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RaceConditions.Run();
+        }
+    }
+}
---
+++ CSharp_Tasks_Revisited/RaceConditions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharp_Tasks_Revisited
+{
+    public class RaceConditions
+    {
+        private static int counter = 0;
+
+        public static void Run()
+        {
+            Task t1 = Task.Factory.StartNew(RunProblem);
+            Task t2 = t1.ContinueWith(ante => RunSolutionWithContinueWith());
+            Task.WaitAll(new Task[] { t1, t2 });
+
+            Console.ReadLine();
+        }
+
+        private static void RunProblem()
+        {
+            Print("The Problem");
+            Task.Run(() => PrintNumbers());
+            Task.Run(() => PrintLetters());
+        }
+
+
+        private static void RunSolutionWithContinueWith()
+        {
+            Print("'ContinueWith' Solution");
+            Task t1 = Task.Factory.StartNew(PrintNumbers);
+            Task t2 = t1.ContinueWith(ante => PrintLetters());
+            Task.WaitAll(new Task[] { t1, t2 });
+        }
+
+        private static void PrintNumbers()
+        {
+            for (counter = 1; counter <= 5; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+
+        private static void PrintLetters()
+        {
+            for (counter = 65; counter <= 70; counter++)
+            {
+                char chr = (char)counter;
+                Console.WriteLine(chr);
+            }
+        }
+
+        private static void Print(string title)
+        {
+            string divider = new string('-', 100);
+            Console.WriteLine();
+            Console.WriteLine(divider);
+            Console.WriteLine(title);
+            Console.WriteLine(divider);
+        }
+    }
+}
